c8e4694810d3553b16ccf2370cf1faa820cb1dc3
Edgar Calderon <edgarcalderon28@hotmail.com>
2022-11-09 13:11:18 -0500

    Get popular series - page and bloc implementation

--- lib/presentation/bloc/home/home_bloc.dart
+++ lib/presentation/bloc/home/home_bloc.dart
@@ -1,19 +1,35 @@
-
 import 'package:injectable/injectable.dart';
+import 'package:moviestest/domain/models/serie.dart';
 import 'package:moviestest/domain/use_case/authentication_use_case.dart';
+import 'package:moviestest/domain/use_case/series_use_case.dart';
 import 'package:moviestest/presentation/bloc/provider/bloc.dart';
+import 'package:rxdart/streams.dart';
+import 'package:rxdart/subjects.dart';
 
 @injectable
 class HomeBloc extends Bloc {
   final AuthenticationUseCase _authenticationUseCase;
+  final SeriesUseCase _seriesUseCase;
 
-  HomeBloc(this._authenticationUseCase);
+  HomeBloc(this._authenticationUseCase, this._seriesUseCase);
 
-  void logout(){
+  final BehaviorSubject<List<Serie>> _popularSeriesSubject =
+      BehaviorSubject<List<Serie>>();
+
+  ValueStream<List<Serie>> get popularSeriesStream =>
+      _popularSeriesSubject.stream;
+
+  Future<void> getSeries() async {
+    final List<Serie> series = await _seriesUseCase.getPopular();
+    _popularSeriesSubject.value = series;
+  }
+
+  void logout() {
     _authenticationUseCase.logout();
   }
 
-
   @override
-  void dispose() {}
+  void dispose() {
+    _popularSeriesSubject.close();
+  }
 }
--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moviestest/domain/models/serie.dart';
 import 'package:moviestest/presentation/bloc/home/home_bloc.dart';
 import 'package:moviestest/presentation/state/base_state.dart';
 
@@ -10,6 +11,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends BaseState<HomePage, HomeBloc> {
+  @override
+  void initState() {
+    super.initState();
+    bloc!.getSeries();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +30,64 @@ class _HomePageState extends BaseState<HomePage, HomeBloc> {
               icon: const Icon(Icons.logout))
         ],
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text('Popular'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          StreamBuilder<List<Serie>>(
+              stream: bloc!.popularSeriesStream,
+              builder: (context, seriesSnapshot) {
+                if (seriesSnapshot.hasData) {
+                  final List<Serie> populars = seriesSnapshot.data!;
+                  return SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        ...populars.map<Widget>(
+                          (Serie serie) {
+    
+                            return Container(
+                              margin: EdgeInsets.only(
+                                right:
+                                    populars.indexOf(serie) != populars.length -1
+                                        ? 20
+                                        : 0,
+                              ),
+                              child: Image.network(
+                                serie.getPosterImageUrl(
+                                  type: SerieImageType.w200,
+                                ),
+                                height: 150,
+                              ),
+                            );
+                          },
+                        ).toList(),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                      ],
+                    ),
+                  );
+                }
+
+                return const Center(child: CircularProgressIndicator());
+              })
+        ],
+      ),
+
     );
   }
 }
